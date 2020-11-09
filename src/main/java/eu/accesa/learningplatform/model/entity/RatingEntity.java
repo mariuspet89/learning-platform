34d3be82dcc2d00dd961527c9cd6a1116bd4ea87
Oana Pantea <oana.pantea@accesa.eu>
2020-11-09 22:31:56 +0200

    Create Rating entity and rating table

---
+++ src/main/java/eu/accesa/learningplatform/model/entity/RatingEntity.java
@@ -0,0 +1,59 @@
+package eu.accesa.learningplatform.model.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "RATINGS")
+public class RatingEntity {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "NO_OF_STARS")
+    private Integer noOfStars;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getNoOfStars() {
+        return noOfStars;
+    }
+
+    public void setNoOfStars(Integer noOfStars) {
+        this.noOfStars = noOfStars;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RatingEntity rating = (RatingEntity) o;
+        return id.equals(rating.id) &&
+                noOfStars.equals(rating.noOfStars) &&
+                description.equals(rating.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, noOfStars, description);
+    }
+}
