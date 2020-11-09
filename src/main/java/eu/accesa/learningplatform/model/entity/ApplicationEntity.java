0a5a8e4d428896e241234cea86ec2cbe059fc5de
Cristina Popescu <cristina.popescu@accesa.eu>
2020-11-09 16:47:37 +0200

    Application entity feature

---
+++ src/main/java/eu/accesa/learningplatform/model/entity/ApplicationEntity.java
@@ -0,0 +1,59 @@
+package eu.accesa.learningplatform.model.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Table(name = "APPLICATION")
+public class ApplicationEntity {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+
+    @Column(name = "STATUS")
+    private String status;
+
+    @Column(name = "COURSE_IDEA")
+    private String courseIdea;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCourseIdea() {
+        return courseIdea;
+    }
+
+    public void setCourseIdea(String courseIdea) {
+        this.courseIdea = courseIdea;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ApplicationEntity)) return false;
+        ApplicationEntity that = (ApplicationEntity) o;
+        return getId().equals(that.getId()) &&
+                Objects.equals(getStatus(), that.getStatus()) &&
+                Objects.equals(getCourseIdea(), that.getCourseIdea());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getStatus(), getCourseIdea());
+    }
+}
