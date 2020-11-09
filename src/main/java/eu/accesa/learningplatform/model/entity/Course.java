51e3493a4a100b2d07856d2530b63ff391a83664
marius.petrut <marius.petrut@accesa.eu>
2020-11-09 17:55:38 +0200

    create course entity

---
+++ src/main/java/eu/accesa/learningplatform/model/entity/Course.java
@@ -0,0 +1,70 @@
+package eu.accesa.learningplatform.model.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "COURSE")
+public class Course {
+
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "DESCRIPTION")
+    private String description;
+    @Column(name = "TOTAL_DURATION")
+    private Double totalDuration;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getTotalDuration() {
+        return totalDuration;
+    }
+
+    public void setTotalDuration(Double totalDuration) {
+        this.totalDuration = totalDuration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(id, course.id) &&
+                Objects.equals(name, course.name) &&
+                Objects.equals(description, course.description) &&
+                Objects.equals(totalDuration, course.totalDuration);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, totalDuration);
+    }
+}
