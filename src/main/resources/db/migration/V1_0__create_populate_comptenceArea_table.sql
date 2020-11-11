94a1c828002579bbb79d6d01b8f40408dade0f1b
marius.petrut <marius.petrut@accesa.eu>
2020-11-11 15:55:30 +0200

    Revert "deleted competence area script"
    This reverts commit bfd86cd480fd6313f503d763f8f13f68bcf1905c.

---
+++ src/main/resources/db/migration/V1_0__create_populate_comptenceArea_table.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE IF NOT EXISTS COMPETENCE_AREA (
+
+    ID bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    NAME varchar(30)
+);
+insert ignore into COMPETENCE_AREA values(null, 'Project Management');
+insert ignore into COMPETENCE_AREA values(null, 'Product Design');
+insert ignore into COMPETENCE_AREA values(null, 'Software Architecture');
+insert ignore into COMPETENCE_AREA values(null, 'Quality Assurance');
+insert ignore into COMPETENCE_AREA values(null, 'System Operations');
+insert ignore into COMPETENCE_AREA values(null, 'Java Technology');
+insert ignore into COMPETENCE_AREA values(null, 'Web & mobile');
+insert ignore into COMPETENCE_AREA values(null, '.NET Technology');
