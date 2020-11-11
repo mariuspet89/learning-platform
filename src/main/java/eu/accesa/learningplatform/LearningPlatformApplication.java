fef0474fdc270f2c17444bdcdb7fc93e70d70d29
Raluca Gabor <raluca.gabor@accesa.eu>
2020-11-11 17:58:57 +0200

    added model mapper bean

--- src/main/java/eu/accesa/learningplatform/LearningPlatformApplication.java
+++ src/main/java/eu/accesa/learningplatform/LearningPlatformApplication.java
@@ -1,5 +1,6 @@
 package eu.accesa.learningplatform;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -24,5 +25,9 @@ public class LearningPlatformApplication {
                         .description("Spring training api description")
                         .termsOfService("http://swagger.io/terms/")
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
