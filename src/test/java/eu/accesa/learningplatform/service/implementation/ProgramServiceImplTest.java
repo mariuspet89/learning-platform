265672a0eb0dc8a0a350069d037a5c4fda5a3c7d
cristina.popescu <cristina.popescu@accesa.eu>
2020-12-03 14:39:34 +0200

    tests for get and delete

--- src/test/java/eu/accesa/learningplatform/service/implementation/ProgramServiceImplTest.java
+++ src/test/java/eu/accesa/learningplatform/service/implementation/ProgramServiceImplTest.java
@@ -17,7 +17,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static eu.accesa.learningplatform.utils.TestUtils.*;
 import static eu.accesa.learningplatform.utils.TestUtils.testUtilsLessonDtoNoId;
@@ -81,18 +84,57 @@ class ProgramServiceImplTest {
     }
 
     @Test
-    public void deleteProgram(){
-        Long id = 3l;
-        CompetenceAreaEntity competenceAreaEntity = TestUtils.createCompetenceArea(2l);
-        ProgramEntity programEntity = TestUtils.initializeProgram("Java Internship",
+    public void getPrograms() {
+        when(programRepository.findAll()).thenReturn(programList());
+        final List<ProgramDto> programsFound = programServiceImpl.findAllPrograms();
+        assertNotNull(programsFound, "List is empty");
+        assertEquals(programsFound.size(), 2, "List size doesn't match actual size");
+        verify(programRepository).findAll();
+    }
+
+/*    @Test
+    public void updateProgram() {
+        CompetenceAreaEntity competenceAreaEntity = createCompetenceArea(2L);
+        ProgramEntity programEntity = initializeProgram(1L,
+                "Java Internship",
                 "aa",
                 LocalDate.parse("2020-01-08"),
                 LocalDate.parse("2020-01-15"),
                 competenceAreaEntity);
-//        UserEntity userEntity = TestUtils.createUserEntity("a@gmail.com", "a@gmail.com", "a@gmail.com", "a@gmail.com", "a@gmail.com");
-//        programEntity.getUserEntities().add(userEntity);
-//        programEntity.getUserEntities().add(userEntity);
-//        programEntity.getUserEntities().add(userEntity);
+        CompetenceAreaEntity updatedCompetenceArea = createCompetenceArea(1L);
+        ProgramEntity updatedProgram = initializeProgram(programEntity.getId(),
+                "Java Summer Internship",
+                "hhh",
+                LocalDate.parse("2020-07-08"),
+                LocalDate.parse("2020-09-15"),
+                updatedCompetenceArea);
+        when(programRepository.findById(programEntity.getId())).thenReturn(Optional.of(programEntity));
+        when(competenceAreaRepository.findById(programEntity.getCompetenceAreaEntity().getId())).thenReturn(Optional.of(competenceAreaEntity));
+        when(programRepository.save(programEntity)).thenReturn(updatedProgram);
+        ProgramDto programDto = initializeProgramDto(updatedProgram.getId(),
+                updatedProgram.getProgramName(),
+                updatedProgram.getDescription(),
+                updatedProgram.getStartDate(),
+                updatedProgram.getEndDate(),
+                updatedCompetenceArea.getId());
+        ProgramDto updatedProgramDto = programServiceImpl.updateProgram(programDto, programDto.getId());
+        assertNotNull(updatedProgramDto);
+        assertEquals(updatedProgramDto.getProgramName(), "Java Summer Internship", "field name doesn't match");
+    }*/
+
+    @Test
+    public void deleteProgram(){
+        Long id = 3l;
+        CompetenceAreaEntity competenceAreaEntity = TestUtils.createCompetenceArea(2l);
+        UserEntity userEntity = TestUtils.createUserEntity("a@gmail.com", "a@gmail.com", "a@gmail.com", "a@gmail.com", "a@gmail.com");
+        Set<UserEntity> userEntities = new HashSet<>();
+        userEntities.add(userEntity);
+        ProgramEntity programEntity = TestUtils.initializeProgramWithUser(3L,"Java Internship",
+                "aa",
+                LocalDate.parse("2020-01-08"),
+                LocalDate.parse("2020-01-15"),
+                competenceAreaEntity,
+                userEntities);
         when(programRepository.findById(id)).thenReturn(Optional.of(programEntity));
         Long idToDelete = 3l;
         programServiceImpl.deleteProgram(idToDelete);
--- src/test/java/eu/accesa/learningplatform/utils/TestUtils.java
+++ src/test/java/eu/accesa/learningplatform/utils/TestUtils.java
@@ -6,7 +6,9 @@ import eu.accesa.learningplatform.model.entity.*;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TestUtils {
     public static LessonDto testUtilsLessonDto(Long id, String name, double duration, Long courseId) {
@@ -82,8 +84,31 @@ public class TestUtils {
         return programEntity;
     }
 
+    public static ProgramEntity initializeProgramWithUser(Long id, String name, String desc, LocalDate startDate, LocalDate endDate, CompetenceAreaEntity competenceAreaEntity, Set<UserEntity> userEntities){
+        ProgramEntity programEntity = new ProgramEntity();
+        programEntity.setId(id);
+        programEntity.setProgramName(name);
+        programEntity.setDescription(desc);
+        programEntity.setStartDate(startDate);
+        programEntity.setEndDate(endDate);
+        programEntity.setCompetenceAreaEntity(competenceAreaEntity);
+        programEntity.setUserEntities(userEntities);
+        return programEntity;
+    }
+
     public static ProgramDto initializeProgramDto(String name, String desc, LocalDate startDate, LocalDate endDate, Long cmpAreaId){
         ProgramDto programDto = new ProgramDto();
+        programDto.setProgramName(name);
+        programDto.setDescription(desc);
+        programDto.setStartDate(startDate);
+        programDto.setEndDate(endDate);
+        programDto.setCompetenceAreaId(cmpAreaId);
+        return programDto;
+    }
+
+    public static ProgramDto initializeProgramDto(Long id, String name, String desc, LocalDate startDate, LocalDate endDate, Long cmpAreaId){
+        ProgramDto programDto = new ProgramDto();
+        programDto.setId(id);
         programDto.setProgramName(name);
         programDto.setDescription(desc);
         programDto.setStartDate(startDate);
@@ -99,12 +124,38 @@ public class TestUtils {
     }
 
     public static UserEntity createUserEntity(String a, String b, String c, String d, String e){
+        ProgramEntity programEntity = initializeProgram(3L,"Java Internship",
+                "aa",
+                LocalDate.parse("2020-01-08"),
+                LocalDate.parse("2020-01-15"),
+                createCompetenceArea(1L));
+        Set<ProgramEntity> programEntities = new HashSet<>();
+        programEntities.add(programEntity);
         UserEntity userEntity = new UserEntity();
         userEntity.setImageUrl(a);
         userEntity.setEmail(b);
         userEntity.setFirstName(c);
         userEntity.setLastName(d);
         userEntity.setPassword(e);
+        userEntity.setProgramEntities(programEntities);
         return userEntity;
+    }
+
+    public static List<ProgramEntity> programList() {
+        return Arrays.asList(
+                initializeProgram(1L,
+                        "Java Internship",
+                        "aa",
+                        LocalDate.parse("2020-01-08"),
+                        LocalDate.parse("2020-01-15"),
+                        createCompetenceArea(1L)),
+                initializeProgram(2L,
+                        "AI Internship",
+                        "bb",
+                        LocalDate.parse("2020-01-10"),
+                        LocalDate.parse("2020-05-15"),
+                        createCompetenceArea(1L))
+                );
+
     }
 }
