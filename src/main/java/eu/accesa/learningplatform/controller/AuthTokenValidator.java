73ae9dd7836f4f64dcdda481a01a2d6ffb1075d6
Radu Rusu <radu.rusu@accesa.eu>
2020-12-10 21:33:57 +0200

    Fix request filtering

--- src/main/java/eu/accesa/learningplatform/controller/AuthTokenValidator.java
+++ src/main/java/eu/accesa/learningplatform/controller/AuthTokenValidator.java
@@ -2,6 +2,8 @@ package eu.accesa.learningplatform.controller;
 
 import eu.accesa.learningplatform.service.TokenService;
 import eu.accesa.learningplatform.util.AuthUtils;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -28,35 +30,27 @@ public class AuthTokenValidator implements Filter {
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 
         String authorization = httpServletRequest.getHeader("Authorization");
-        String token = AuthUtils.extractBearerToken(authorization);
+        String bearer = AuthUtils.extractBearerToken(authorization);
 
         boolean userHasAuthorisation = false;
 
-        //TODO: Treat empty authorisation header allowing requests to be made
-
-        if (token != null && !token.isEmpty()) {
-            if (!tokenService.verifyToken(token)) {
-                httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN,
-                        "Invalid token. Please login again");
-                userHasAuthorisation = false;
-            } else {
-                userHasAuthorisation = true;
-            }
+        if (StringUtils.isNotEmpty(authorization)) {
+            if (tokenService.verifyToken(bearer)) {
+            	userHasAuthorisation = true;
+            } 
         }
-
-        if (httpServletRequest.getRequestURL().indexOf("/authentication/token") != -1) {
+        if (!(httpServletRequest.getRequestURL().indexOf("/authentication/token") == -1)) {
             userHasAuthorisation = true;
         }
-
-        if (httpServletRequest.getRequestURL().indexOf("/swagger-ui/index.html") != -1) {
+        if (!(httpServletRequest.getRequestURL().indexOf("/swagger-ui/index.html") == -1)) {
             userHasAuthorisation = true;
         }
-
-        if (!userHasAuthorisation) {
-            httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN,
+        if (userHasAuthorisation) {
+        	filterChain.doFilter(request, httpServletResponse);
+        } else {
+        	httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED,
                     "Unauthorized. Please login before making a request.");
         }
-        filterChain.doFilter(request, httpServletResponse);
     }
 
     @Override
