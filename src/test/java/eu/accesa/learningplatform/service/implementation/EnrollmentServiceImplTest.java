5f973bcfa6d8e144ae2dddfe5ad295235157023b
marius.petrut <marius.petrut@accesa.eu>
2020-12-10 13:11:13 +0200

    Test setup

---
+++ src/test/java/eu/accesa/learningplatform/service/implementation/EnrollmentServiceImplTest.java
@@ -0,0 +1,20 @@
+package eu.accesa.learningplatform.service.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnrollmentServiceImplTest {
+
+    @Test
+    public void enroll() {
+    }
+
+    @Test
+    public void undoEnroll() {
+    }
+
+    @Test
+    public void getAllEnrollments() {
+    }
+}
